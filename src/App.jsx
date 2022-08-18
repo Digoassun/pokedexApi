20e3af1eb5be068a0df8cba91811d5d1e70e63d3
Digoassun <digoassun@gmail.com>
2022-08-18 20:08:56 -0300

    Alteração de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import S from "./App.module.css";
 function App() {
 
   const [infos, setInfos] = useState();
-  console.log()
   return (
     <div className={S.app}>
       <Form setInfos={setInfos}/>
       <Card
-        name={infos? infos.forms[0].name : ""}
-        img={infos? infos.sprites.other.dream_world.front_default : ""}
-        type={infos? infos.types[0].type.name: "" }
+        name    
+        img={infos? infos.sprites.other["official-artwork"].front_default : ""}
+        type={infos? infos.types[0].type.name.toUpperCase() : "" }
       />
     </div>
   );
